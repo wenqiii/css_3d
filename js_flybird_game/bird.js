69635015a66d2ebe785946abd25540a4f5d42443
wenqiii <1355174654@qq.com>
2017-08-16 23:43:06 +0800

    Add files via upload

---
+++ js_flybird_game/bird.js
@@ -0,0 +1,57 @@
+var bird = {
+  div: document.createElement('div'),
+  fallSpeed: 0,
+  flyTimer: null,
+  wingTimer: null,
+  showBird: function(parentObj) {
+    this.div.style.width = '40px';
+    this.div.style.height = '28px';
+    this.div.style.backgroundImage = 'url(/img/bird0.png)';
+    this.div.style.backroundRepeat = 'no-repeat';
+    this.div.style.position = 'absolute';
+    this.div.style.left = '50px';
+    this.div.style.top = '200px';
+    this.div.style.zIndex = '1';
+    parentObj.appendChild(this.div);
+  },
+  flyBird: function() {
+    this.flyTimer = setInterval(fly,40);
+    var that = this;
+    function fly () {
+      that.div.style.top = that.div.offsetTop + that.fallSpeed++ + 'px';
+      if(that.div.offsetTop < 0) {
+        that.fallSpeed = 2;
+      }
+      if(that.div.offsetTop >= 395) {
+        that.fallSpeed = 0;
+        clearInterval(that.flyTimer);
+        clearInterval(that.wingTimer);
+      }
+      if(that.fallSpeed > 12) {
+        that.fallSpeed = 12;
+      }
+    }
+  },
+  wingWave: function() {
+    var up = ["url(img/up_bird0.png)","url(img/up_bird1.png)"]
+    var down = ["url(img/down_bird0.png)","url(img/down_bird1.png)"]
+    var i = 0,j = 0;
+    this.wingTimer = setInterval(wing,120);
+    var that = this;
+    function wing() {
+      if (that.fallSpeed > 0) {
+              that.div.style.backgroundImage = down[i++];
+              if (i >= 2) {
+                  i = 0;
+              }
+          } else {
+              that.div.style.backgroundImage = up[j++];
+              if (j >= 2) {
+                  j = 0;
+              }
+          }
+
+      // 向上飞。向下降
+    }
+  }
+}
